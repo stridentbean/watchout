d3e3c1e74371d92f4be3d915972bcf719fc7939e
stridentbean <borglin.me@gmail.com>
2015-05-22 15:41:42 -0700

    Randomly generates new positions + transitions

--- watchout.js
+++ watchout.js
@@ -1,5 +1,6 @@
 // start slingin' some d3 here.
 
+//declaring and instantiating variables
 var gameBoard = d3.select('.gameBoard').node().getBoundingClientRect();
 
 d3.select('.gameBoard')
@@ -9,15 +10,51 @@ d3.select('.gameBoard')
     width: gameBoard.width - 10
   });
 
-d3.selectAll('svg')
-  .append("circle")
-  .attr("cx", 300)
-  .attr("cy", 300)
-  .attr("r", 50)
-  .style("fill", "blue");
 
-d3.selectAll("circle")
-  .transition()
-  .duration(4000)
-  .attr("cx", 600)
-  .attr("cy", 600);
+var createAsteroid = function(x, y, diameter) {
+
+  d3.selectAll('svg')
+    .append("image")
+    .attr("x", x)
+    .attr("y", y)
+    .attr("xlink:href", "asteroid.png")
+    .attr("height", diameter)
+    .attr("width", diameter);
+
+};
+
+var generateRandomAsteroid = function(num) {
+  for(var i = 0; i < num; i++) {
+    var x = Math.floor(Math.random() * gameBoard.width);
+    var y = Math.floor(Math.random() * gameBoard.height);
+    var diameter = Math.floor(Math.random() * 50);
+    createAsteroid(x, y, diameter);
+  }
+}
+
+generateRandomAsteroid(50);
+
+// d3.selectAll("circle")
+//   .transition()
+//   .duration(4000)
+//   .attr("cx", 100)
+//   .attr("cy", 100);
+
+/* Starting the simulation */
+var randomGenerator = function(maxSize) {
+  return function() {
+    return Math.floor(Math.random() * maxSize);
+  }
+}
+
+var randomlyMoveAsteroid = function() {
+  //var x = Math.floor(Math.random() * gameBoard.width);
+  //var y = Math.floor(Math.random() * gameBoard.height);
+  d3.selectAll('image')
+    .transition()
+    .duration(2000)
+    .attr('x', randomGenerator(gameBoard.width))
+    .attr('y', randomGenerator(gameBoard.height));
+};
+
+setInterval(randomlyMoveAsteroid, 2000);
