fbeae7cf0729b54e77a75228d649ad908a0fe1a1
stridentbean <borglin.me@gmail.com>
2015-05-22 15:12:46 -0700

    Added svg, circle, and implemented our first circle

--- watchout.js
+++ watchout.js
@@ -1,2 +1,23 @@
 // start slingin' some d3 here.
 
+var gameBoard = d3.select('.gameBoard').node().getBoundingClientRect();
+
+d3.select('.gameBoard')
+  .append("svg")
+  .attr({
+    height: gameBoard.height - 10,
+    width: gameBoard.width - 10
+  });
+
+d3.selectAll('svg')
+  .append("circle")
+  .attr("cx", 300)
+  .attr("cy", 300)
+  .attr("r", 50)
+  .style("fill", "blue");
+
+d3.selectAll("circle")
+  .transition()
+  .duration(4000)
+  .attr("cx", 600)
+  .attr("cy", 600);
